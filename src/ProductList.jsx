5c80aa390bf7760ff69493be201ca8f9f285d932
jylep <jyl44760@gmail.com>
2025-11-06 23:46:28 +0100

    feat: add plants

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import './ProductList.css'
+import { useState } from 'react';
+import './ProductList.css';
 import CartItem from './CartItem';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
@@ -220,18 +220,18 @@ function ProductList({ onHomeClick }) {
         justifyContent: 'space-between',
         alignIems: 'center',
         fontSize: '20px',
-    }
+    };
     const styleObjUl = {
         display: 'flex',
         justifyContent: 'space-between',
         alignItems: 'center',
         width: '1100px',
-    }
+    };
     const styleA = {
         color: 'white',
         fontSize: '30px',
         textDecoration: 'none',
-    }
+    };
 
     const handleHomeClick = (e) => {
         e.preventDefault();
@@ -247,39 +247,61 @@ function ProductList({ onHomeClick }) {
         setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
         setShowCart(false); // Hide the cart when navigating to About Us
     };
-
+    const handleAddToCartClick = (e, { plantName, price }) => {
+        e.preventDefault();
+        alert(`${plantName} (${price}) was added to cart`);
+    };
     const handleContinueShopping = (e) => {
         e.preventDefault();
         setShowCart(false);
     };
     return (
         <div>
-            <div className="navbar" style={styleObj}>
+            <div className="navbar" style={ styleObj }>
                 <div className="tag">
                     <div className="luxury">
                         <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
-                        <a href="/" onClick={(e) => handleHomeClick(e)}>
+                        <a href="/" onClick={ (e) => handleHomeClick(e) }>
                             <div>
-                                <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
-                                <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
+                                <h3 style={ { color: 'white' } }>Paradise Nursery</h3>
+                                <i style={ { color: 'white' } }>Where Green Meets Serenity</i>
                             </div>
                         </a>
                     </div>
 
                 </div>
-                <div style={styleObjUl}>
-                    <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                <div style={ styleObjUl }>
+                    <div> <a href="#" onClick={ (e) => handlePlantsClick(e) } style={ styleA }>Plants</a></div>
+                    <div> <a href="#" onClick={ (e) => handleCartClick(e) } style={ styleA }><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
                 </div>
             </div>
-            {!showCart ? (
+            { !showCart ? (
                 <div className="product-grid">
 
+                    { plantsArray.map(category =>
+                        <div key={ category.category }>
+                            <h1>{ category.category }</h1>
+                            <div className="product-list">
+                                { category.plants.map(plant =>
+                                    <div key={ plant.name } className="product-card">
+                                        <img className="product-image" src={ plant.image } alt={ `Image representing ${plant.name}` } />
+                                        <footer>
+                                            <h2 className="product-title">{ plant.name }</h2>
+                                            <p className="product-description">{ plant.description }</p>
+                                            <div className="product-cost">{ plant.price }</div>
+                                            <button className="product-button" onClick={ (e) => handleAddToCartClick(e, { plantName: plant.name, price: plant.price }) }>Add to cart</button>
+                                        </footer>
+                                    </div>)
+                                }
+                            </div>
+
+                        </div>)
+                    }
 
                 </div>
             ) : (
-                <CartItem onContinueShopping={handleContinueShopping} />
-            )}
+                <CartItem onContinueShopping={ handleContinueShopping } />
+            ) }
         </div>
     );
 }
