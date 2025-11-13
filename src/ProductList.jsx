75d60a533c3044d8fa988af3ea657d46d885313a
jylep <jyl44760@gmail.com>
2025-11-13 14:26:20 +0100

    fix: add the cart count

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import './ProductList.css';
 import CartItem from './CartItem';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addItem } from './CartSlice';
 import classNames from 'classnames';
 
 function ProductList({ onHomeClick }) {
     const dispatch = useDispatch();
+    const cart = useSelector(state => state.cart.items);
     const [showCart, setShowCart] = useState(false);
     const [, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
@@ -282,7 +283,14 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={ styleObjUl }>
                     <div> <a href="#" onClick={ (e) => handlePlantsClick(e) } style={ styleA }>Plants</a></div>
-                    <div> <a href="#" onClick={ (e) => handleCartClick(e) } style={ styleA }><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div>
+                        <a href="#" onClick={ (e) => handleCartClick(e) } style={ styleA }>
+                            <h1 className='cart'>
+                                <span className="cart_quantity_count">{cart.length}</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path></svg>
+                            </h1>
+                        </a>
+                    </div>
                 </div>
             </div>
             { !showCart ? (
